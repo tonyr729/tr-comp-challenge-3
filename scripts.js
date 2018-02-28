a375f59f717cf55044ebeb1badb5f386cd825bd8
Tony Robertson <tonyr729@gmail.com>
2018-02-27 18:20:10 -0700

    Finish media queries and finalize
    Finshed media queries for all sizes.
    Cleaned up HTML/CSS to idomatic style.
    Added a few JS functionality to the site.

---
+++ scripts.js
@@ -0,0 +1,42 @@
+$('.main__button-collapse').on('click', collapseGraph);
+$('.main__button-show').on('click', collapseGraph);
+$('.nav__button-hamburger').on('click', asideToggle);
+
+var blah = true;
+
+function collapseGraph() {
+  console.log('working');
+  if (blah === true) {
+    blah = false;
+    $('.main__section-graphs').hide();
+    $('.main__button-collapse').hide();
+    $('.main__button-show').show();
+  } else {
+    blah = true;
+    $('.main__section-graphs').show();
+    $('.main__button-collapse').show();
+    $('.main__button-show').hide();
+  }
+}
+
+function asideToggle() {
+  if($('.aside').css('display') === "none") {
+    expandAside();
+  } else {
+    collapseAside();
+  }
+}
+
+function collapseAside() {
+  $('.aside').hide();
+  $('.nav').css({ "grid-column-start": "1"})
+  $('.header').css({ "grid-column-start": "1"})
+  $('.main').css({ "grid-column-start": "1"})
+}
+
+function expandAside() {
+  $('.aside').show();
+  $('.nav').css({ "grid-column-start": "2"})
+  $('.header').css({ "grid-column-start": "2"})
+  $('.main').css({ "grid-column-start": "2"})
+}
